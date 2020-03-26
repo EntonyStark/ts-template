f472345960d087b1ec97ad889c8bb13e3b5fce9d
EntonyStark <leonskottkenedi@gmail.com>
2020-03-26 23:11:49 +0200

    rm unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
-import { makeObjectReadOnly } from './utils/readOnly';
 import { isBoolean } from './utils/isBoolean';
 
-const person = {
-  name: 'Tony',
-};
-const myself = makeObjectReadOnly(person);
-console.log(`Hello World from index.ts i am ${myself.name}`);
-
 export default {
-  isBoolean
+  isBoolean,
 };
